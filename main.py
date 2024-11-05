fd1bc70669c1c4e5f2aa82f605cd6575c0ea975e
The-Olive <lpierrelouis17@mail.valenciacollege.edu>
2024-11-04 22:52:36 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class Players:
+    def __init__(self, name, position):
+        self.name = playerList
+        self.position = position
+    def __str__(self):
+        return f"{self.name} {self.position}"
+
+
+
+playerList = [
+    "Joe Montana",
+    "Barry Sanders",
+    "Jerrie Rice",
+    "Graham Gano K",
+]
+
+player1 = (playerList, "QB")
